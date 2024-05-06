1e2d640a279e0bd76ca2e3b6db965ba85dd61d52
Niyas Ny <niyasny433@gmail.com>
2024-05-06 20:39:21 +0530

    canteen page refactoring

--- lib/app/view/canteenpage/pages/home.dart
+++ lib/app/view/canteenpage/pages/home.dart
@@ -20,6 +20,7 @@ class _CanteenHomeState extends State<CanteenHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       body: Container(
         margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 10.0),
         child: Column(
