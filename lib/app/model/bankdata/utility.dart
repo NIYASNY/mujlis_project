6d605d784700624b0346e44b7039e7cf8627c090
Niyas Ny <niyasny433@gmail.com>
2024-05-17 17:20:11 +0530

    fixed bank page error

--- lib/app/model/bankdata/utility.dart
+++ lib/app/model/bankdata/utility.dart
@@ -5,13 +5,22 @@ int totals = 0;
 
 final box = Hive.box<Add_data>('data');
 
+int parseAmount(String amount) {
+  try {
+    return int.parse(amount);
+  } catch (e) {
+    print('Error parsing amount: $amount');
+    return 0; // Or handle the error as needed
+  }
+}
+
 int total() {
   var history2 = box.values.toList();
-  List a = [0, 0];
+  List<int> a = [0, 0];
   for (var i = 0; i < history2.length; i++) {
     a.add(history2[i].IN == 'Income'
-        ? int.parse(history2[i].amount)
-        : int.parse(history2[i].amount) * -1);
+        ? parseAmount(history2[i].amount)
+        : parseAmount(history2[i].amount) * -1);
   }
   totals = a.reduce((value, element) => value + element);
   return totals;
@@ -19,9 +28,9 @@ int total() {
 
 int income() {
   var history2 = box.values.toList();
-  List a = [0, 0];
+  List<int> a = [0, 0];
   for (var i = 0; i < history2.length; i++) {
-    a.add(history2[i].IN == 'Income' ? int.parse(history2[i].amount) : 0);
+    a.add(history2[i].IN == 'Income' ? parseAmount(history2[i].amount) : 0);
   }
   totals = a.reduce((value, element) => value + element);
   return totals;
@@ -29,9 +38,9 @@ int income() {
 
 int expenses() {
   var history2 = box.values.toList();
-  List a = [0, 0];
+  List<int> a = [0, 0];
   for (var i = 0; i < history2.length; i++) {
-    a.add(history2[i].IN == 'Income' ? 0 : int.parse(history2[i].amount) * -1);
+    a.add(history2[i].IN == 'Income' ? 0 : parseAmount(history2[i].amount) * -1);
   }
   totals = a.reduce((value, element) => value + element);
   return totals;
@@ -40,7 +49,7 @@ int expenses() {
 List<Add_data> today() {
   List<Add_data> a = [];
   var history2 = box.values.toList();
-  DateTime date = new DateTime.now();
+  DateTime date = DateTime.now();
   for (var i = 0; i < history2.length; i++) {
     if (history2[i].datetime.day == date.day) {
       a.add(history2[i]);
@@ -51,7 +60,7 @@ List<Add_data> today() {
 
 List<Add_data> week() {
   List<Add_data> a = [];
-  DateTime date = new DateTime.now();
+  DateTime date = DateTime.now();
   var history2 = box.values.toList();
   for (var i = 0; i < history2.length; i++) {
     if (date.day - 7 <= history2[i].datetime.day &&
@@ -65,7 +74,7 @@ List<Add_data> week() {
 List<Add_data> month() {
   List<Add_data> a = [];
   var history2 = box.values.toList();
-  DateTime date = new DateTime.now();
+  DateTime date = DateTime.now();
   for (var i = 0; i < history2.length; i++) {
     if (history2[i].datetime.month == date.month) {
       a.add(history2[i]);
@@ -77,7 +86,7 @@ List<Add_data> month() {
 List<Add_data> year() {
   List<Add_data> a = [];
   var history2 = box.values.toList();
-  DateTime date = new DateTime.now();
+  DateTime date = DateTime.now();
   for (var i = 0; i < history2.length; i++) {
     if (history2[i].datetime.year == date.year) {
       a.add(history2[i]);
@@ -87,20 +96,20 @@ List<Add_data> year() {
 }
 
 int total_chart(List<Add_data> history2) {
-  List a = [0, 0];
+  List<int> a = [0, 0];
 
   for (var i = 0; i < history2.length; i++) {
     a.add(history2[i].IN == 'Income'
-        ? int.parse(history2[i].amount)
-        : int.parse(history2[i].amount) * -1);
+        ? parseAmount(history2[i].amount)
+        : parseAmount(history2[i].amount) * -1);
   }
   totals = a.reduce((value, element) => value + element);
   return totals;
 }
 
-List time(List<Add_data> history2, bool hour) {
+List<int> time(List<Add_data> history2, bool hour) {
   List<Add_data> a = [];
-  List total = [];
+  List<int> total = [];
   int counter = 0;
   for (var c = 0; c < history2.length; c++) {
     for (var i = c; i < history2.length; i++) {
