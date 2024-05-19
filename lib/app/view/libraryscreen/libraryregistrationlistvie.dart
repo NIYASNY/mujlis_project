49089e35d3d291bf056c66d71d4f971931e7be99
Niyas Ny <niyasny433@gmail.com>
2024-05-19 18:08:35 +0530

    updated library registration page

--- lib/app/view/libraryscreen/libraryregistrationlistvie.dart
+++ lib/app/view/libraryscreen/libraryregistrationlistvie.dart
@@ -2,6 +2,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
+
+//library registrationpage
 class LibraryRegistrationListView extends StatefulWidget {
   const LibraryRegistrationListView({super.key});
 
