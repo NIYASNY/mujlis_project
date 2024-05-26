f74b309ed8ffbfa1eeb6ca1d937c2e271c1d41c7
Niyas Ny <niyasny433@gmail.com>
2024-05-26 20:50:08 +0530

    updated libaryregister page

--- lib/app/view/libraryscreen/library_book_registrationpage.dart
+++ lib/app/view/libraryscreen/library_book_registrationpage.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
+//librarybookegister page
 class LibraryBookRegistrationPage extends StatefulWidget {
   @override
   _LibraryBookRegistrationPageState createState() =>
