29ac8d85d25f974d4d9f3b870c3a33fe5b94281b
Niyas Ny <niyasny433@gmail.com>
2024-06-01 18:53:23 +0530

    added staffcookpage

--- lib/app/view/staff/staffcook.dart
+++ lib/app/view/staff/staffcook.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+
+//staff cookpage
 class StaffCookPage extends StatefulWidget {
   @override
   _StaffCookPageState createState() => _StaffCookPageState();
