f99f6f361ea06b11306ffeaa94c0e4e8c459f8f0
Niyas Ny <niyasny433@gmail.com>
2024-06-28 21:58:55 +0530

    added assetfolder

--- lib/app/view/widgets/assetfolder.dart
+++ lib/app/view/widgets/assetfolder.dart
@@ -4,7 +4,6 @@ class Assetfolder extends StatelessWidget {
   final String imageside;
   const Assetfolder({super.key, required this.imageside});
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
